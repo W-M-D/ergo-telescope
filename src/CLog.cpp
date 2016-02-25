0a678c83c4bb66044b3f892e894a31a9fb8b1554
neophobia <neophobia@Main.bad>
2016-02-25 09:29:12 -0500

    Updating clog for csv compression

--- src/CLog.cpp
+++ src/CLog.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-
+#include <zlib.h>
 /*
 USAGE
 create a instance of the class
@@ -27,60 +27,18 @@ Log->add(" your string here %d,%f,..."42,42.42 )
 CLog::CLog()
 {
     system("mkdir -p /etc/ERGO/");
-    last_sent_line_get();
 }
 
 void CLog::data_add(std::string & date, std::string & time, std::string & unit_id, std::string & lat,std::string & lon,std::string & alt,std::string & nanoseconds)
 {
     std::stringstream uncompressed_string;
-    std::ofstream data_file;
+    gzFile data_file =  gzopen("/etc/ERGO/ERGO_DATA.csv.gz", "a9h");
     uncompressed_string << date << ' ' << time << ' ' << unit_id << ' ' << lat << ' ' << lon << ' ' << alt << ' ' << nanoseconds << '\n';
     
-    data_file.open("/etc/ERGO/ERGO_DATA.csv",std::ios_base::out | std::ios_base::app);
-    data_file.flush();
-    data_file.close();
+   
+    gzprintf(data_file ,"%s", uncompressed_string.str() );
+    gzclose(data_file);
 }
-
-std::__cxx11::string CLog::compress_string(std::__cxx11::string& uncompressed_string, int compression_level)
-{
-    z_stream zs;                        // z_stream is zlib's control structure
-    memset(&zs, 0, sizeof(zs));
-
-    if (deflateInit(&zs, compression_level) != Z_OK)
-        throw(std::runtime_error("deflateInit failed while compressing."));
-
-    zs.next_in = (Bytef*)uncompressed_string.data();
-    zs.avail_in = uncompressed_string.size();           // set the z_stream's input
-
-    int ret;
-    char outbuffer[32768];
-    std::string outstring;
-
-    // retrieve the compressed bytes blockwise
-    do {
-        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
-        zs.avail_out = sizeof(outbuffer);
-
-        ret = deflate(&zs, Z_FINISH);
-
-        if (outstring.size() < zs.total_out) {
-            // append the block to the output string
-            outstring.append(outbuffer,
-                             zs.total_out - outstring.size());
-        }
-    } while (ret == Z_OK);
-
-    deflateEnd(&zs);
-
-    if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
-        std::ostringstream oss;
-        oss << "Exception during zlib compression: (" << ret << ") " << zs.msg;
-        throw(std::runtime_error(oss.str()));
-    }
-
-    return outstring;
-}
-
 
 
 void CLog::raw_hex_add(std::deque <uint8_t> & data_list)
