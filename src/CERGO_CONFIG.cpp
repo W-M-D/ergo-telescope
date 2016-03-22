bb9cf465ba6507cb8a6be2d35408b3f97970b34c
Matthew Weger <matthew.d.weger@gmail.com>
2016-03-22 19:06:52 -0400

    adding CERGO_CONFIG

---
+++ src/CERGO_CONFIG.cpp
@@ -0,0 +1,62 @@
+#include "CERGO_CONFIG.h"
+
+CERGO_CONFIG::CERGO_CONFIG()
+{
+
+}
+
+int CERGO_CONFIG::load_config_file()
+{
+  using namespace std;
+  using namespace libconfig; 
+  Config ergo_telescope_config;
+  
+  try
+  {
+    ergo_telescope_config.readFile(config_file_name.c_str());
+  }
+  catch(const FileIOException &fioex)
+  {
+    Log->add("No ergo-telescope config file found starting with default config");
+    return -1;
+  }
+  catch(const ParseException &pex)
+  {
+    Log->add("Error parsing ergo-telescope.cfg at line %d",pex.getLine());
+    return -2;
+  }
+  
+  try
+  {
+    version_number = ergo_telescope_config.lookup("version_number"); 
+  }
+  catch(const SettingNotFoundException &nfex)
+  {
+    Log->add("No version number in config file");
+  }
+  
+  try
+  {
+    GPS_config_file_name = ergo_telescope_config.lookup("GPS_config_file").c_str();
+  }
+  catch(const SettingNotFoundException &nfex)
+  {
+    Log->add("No GPS_config_file_name found in ergo-telescoep.cfg");
+  }
+  
+  try
+  {
+    DEBUG_LEVEL = ergo_telescope_config.lookup("DEBUG_LEVEL");
+  }
+  catch(const SettingNotFoundException &nfex)
+  {
+    Log->add("No debug level found in ergo-telescope.cfg");
+  }
+  
+  return 1;
+}
+
+CERGO_CONFIG::~CERGO_CONFIG()
+{
+
+}
---
+++ src/CERGO_CONFIG.h
@@ -0,0 +1,41 @@
+#ifndef CERGO_CONFIG_H
+#define CERGO_CONFIG_H
+#include <libconfig.h++>
+#include "CLog.h"
+
+
+class CERGO_CONFIG
+{
+public:
+CERGO_CONFIG();
+~CERGO_CONFIG();
+std::string get_GPS_config_file_name()
+{
+  std::string CONFIG_FILE_PATH = "/etc/ERGO";
+  std::string full_config_file_name = config_file_name;
+  full_config_file_name.append(CONFIG_FILE_PATH); 
+  return full_config_file_name;
+}
+
+int get_debug_level()
+{
+  return DEBUG_LEVEL;
+  
+}
+
+double get_version_number()
+{
+  return version_number;
+}
+
+int load_config_file();
+
+private:
+    int DEBUG_LEVEL;
+    std::string GPS_config_file_name;
+    std::string config_file_name="/etc/ERGO/ergo-telescope.cfg";
+    double version_number;
+    CLog * Log;
+};
+
+#endif // CERGO_CONFIG_H
