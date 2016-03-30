a674c3473a2cfa249a993b448008a75db5c17270
Matthew Weger <matthew.d.weger@gmail.com>
2016-03-30 13:56:15 -0400

    adding dealy time to loop

--- src/CERGO_GPIO.cpp
+++ src/CERGO_GPIO.cpp
@@ -62,6 +62,8 @@ int CERGO_GPIO::setdir_gpio(int gpionum)
     std::ofstream setdirgpio(setdir_str.str().c_str()); // open direction file for gpio
     if(!setdirgpio.is_open())
     {
+	std::chrono::seconds delay (1); 
+	std::this_thread::sleep_for( delay );
 	export_gpio(gpionum);
 	setdir_gpio(gpionum);
         return -1;
